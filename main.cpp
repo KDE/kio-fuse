d8a09dd9fb68e9447f0493ae0f4ef9388fc74ea3
Fabian Vogt <fabian@ritter-vogt.de>
2018-12-30 01:00:20 +0100

    Add support for daemonization

--- main.cpp
+++ main.cpp
@@ -13,12 +13,21 @@ int main(int argc, char *argv[])
 	if (fuse_parse_cmdline(&args, &opts) != 0)
 		return 1;
 
-	if (opts.show_help) {
-		printf("usage: %s [options] <mountpoint>\n\n", argv[0]);
+	if(opts.mountpoint == nullptr)
+	{
+		puts("No mountpoint given.");
+		opts.show_help = 1;
+	}
+
+	if (opts.show_help)
+	{
+		printf("Usage: %s [options] <mountpoint>\n\n", argv[0]);
 		fuse_cmdline_help();
 		fuse_lowlevel_help();
 		return 0;
-	} else if (opts.show_version) {
+	}
+	else if (opts.show_version)
+	{
 		printf("FUSE library version %s\n", fuse_pkgversion());
 		fuse_lowlevel_version();
 		return 0;
@@ -28,6 +37,8 @@ int main(int argc, char *argv[])
 	KIOFuseVFS kiofusevfs;
 	if(!kiofusevfs.start(args, opts.mountpoint))
 		return 1;
+
+	fuse_daemonize(opts.foreground);
 
 	fuse_opt_free_args(&args);
 
