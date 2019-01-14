68944455738a61819b82ef08087334fc0e802a6c
Fabian Vogt <fabian@ritter-vogt.de>
2019-01-14 19:42:18 +0100

    Add README and DESIGN files and switch comment type in kiofusevfs.h

--- kiofusevfs.h
+++ kiofusevfs.h
@@ -17,15 +17,17 @@
 namespace KIO { class UDSEntry; }
 
 enum KIOFuseIno : fuse_ino_t {
-	// Defined by the kernel
+	/** Not reserved by the kernel, so used as a marker. */
 	Invalid = 0,
+	/** Defined by the kernel */
 	Root = 1,
 
-	// Fixed by KIOFuse
+	/** The inode number of the parent of deleted nodes. */
 	DeletedRoot,
+	/** The inode number of the _control file. */
 	Control,
 
-	// Dynamic allocation starts here
+	/** Dynamic allocation by insertNode starts here. */
 	DynamicStart,
 };
 
@@ -36,7 +38,9 @@ public:
 	explicit KIOFuseVFS(QObject *parent = nullptr);
 	~KIOFuseVFS();
 
+	/** Mounts the filesystem at mountpoint. Returns true on success. */
 	bool start(fuse_args &args, const char *mountpoint);
+	/** Umounts the filesystem (if necessary) and flushes dirty nodes. */
 	void stop();
 
 public Q_SLOTS:
@@ -69,58 +73,61 @@ private:
 	static void fsync(fuse_req_t req, fuse_ino_t ino, int datasync, struct fuse_file_info *fi);
 
 private:
-	// Returns an empty shared_ptr if not found.
+	/** Returns a pointer to a child node of parent with m_nodeName == name or nullptr. */
 	std::shared_ptr<KIOFuseNode> nodeByName(const std::shared_ptr<KIOFuseNode> &parent, const QString name) const;
-	// Returns an empty shared_ptr if not found.
+	/** Returns a pointer to the KIOFuseNode with inode number ino or nullptr. */
 	std::shared_ptr<KIOFuseNode> nodeForIno(const fuse_ino_t ino) const;
-	// Removes the node from the old parent's children list (if it has a parent) and adds it to the new parent.
+	/** Removes the node from the old parent's children list (if any) and adds it to the new parent's list.*/
 	void reparentNode(const std::shared_ptr<KIOFuseNode> &node, fuse_ino_t newParentIno);
-	// Copies ownership to m_nodes
+	/** Allocates a new inode number, adds node into m_nodes and adds it to the node parent's children list. */
 	fuse_ino_t insertNode(const std::shared_ptr<KIOFuseNode> &node);
-	// Returns the url upwards until a OriginNode is hit.
-	// If no OriginNode is found, an empty QUrl is returned
+	/** Returns the full url upwards until a OriginNode is hit.
+	  * If no OriginNode is found, an empty QUrl is returned. */
 	QUrl remoteUrl(const std::shared_ptr<const KIOFuseNode> &node) const;
-	// Returns the path upwards until a root node.
+	/** Returns the path upwards until a root node. */
 	QString virtualPath(const std::shared_ptr<KIOFuseNode> &node) const;
-	// Fills a (previously zeroed out) struct stat with minimal information
+	/** Fills a (previously zeroed out) struct stat with minimal information about a fake file. */
 	void fillStatForFile(struct stat &attr);
-	// Adjusts the lookup count and deletes the node if it is now zero and a child of DeletedRoot.
+	/** Increments the lookup count of node by delta. */
 	void incrementLookupCount(const std::shared_ptr<KIOFuseNode> &node, uint64_t delta=1);
+	/** Adjusts the lookup count and deletes the node if it is now zero and a child of DeletedRoot. */
 	void decrementLookupCount(const std::shared_ptr<KIOFuseNode> node, uint64_t delta=1);
-	// Depending on the lookup count, it makes the node a child of DeletedRoot or deletes it directly.
+	/** Depending on the lookup count, it makes the node a child of DeletedRoot or deletes it directly. */
 	void markNodeDeleted(const std::shared_ptr<KIOFuseNode> &node);
-	// Sends the struct attr to fuse
+	/** Sends the node's attributes with fuse_reply_attr. */
 	static void replyAttr(fuse_req_t req, std::shared_ptr<KIOFuseNode> node);
-	// Creates a new node on the heap with the matching type and fills m_stat fields.
+	/** Creates a new node with the matching type and fills m_stat fields. */
 	std::shared_ptr<KIOFuseNode> createNodeFromUDSEntry(const KIO::UDSEntry &entry, const fuse_ino_t parentIno, QString nameOverride={});
-	// Invokes callback on error or when the bytes are available for reading/writing.
-	// If the file is not as big, it sets error = ESPIPE.
+	/** Invokes callback on error or when the bytes are available for reading/writing.
+	  * If the file is smaller than bytes, it sets error = ESPIPE. */
 	void waitUntilBytesAvailable(const std::shared_ptr<KIOFuseRemoteFileNode> &node, size_t bytes, std::function<void(int error)> callback);
-	// Invokes callback on error or when all children nodes are available
+	/** Invokes callback on error or when all children nodes are available */
 	void waitUntilChildrenComplete(const std::shared_ptr<KIOFuseDirNode> &node, std::function<void(int error)> callback);
-	// Runs KIO::stat on url and adds a node to the tree if successful. Calls the callback at the end.
+	/** Runs KIO::stat on url and adds a node to the tree if successful. Calls the callback at the end. */
 	void mountUrl(QUrl url, std::function<void(const std::shared_ptr<KIOFuseNode>&, int)> callback);
-	// Handle the _control command in cmd asynchronously and call callback upon completion or failure.
+	/** Handles the _control command in cmd asynchronously and call callback upon completion or failure. */
 	void handleControlCommand(QString cmd, std::function<void(int error)> callback);
-	// Mark a node's cache as dirty
+	/** Marks a node's cache as dirty and add it to m_dirtyNodes. */
 	void markCacheDirty(const std::shared_ptr<KIOFuseRemoteFileNode> &node);
-	// If the cache is dirty, writes the local cache to the remote. Callback is called on success, failure
-	// or if cache was not dirty.
+	/** If the cache is dirty, writes the local cache to the remote. Callback is called on success, failure
+	  * or if cache was not dirty. */
 	void flushRemoteNode(const std::shared_ptr<KIOFuseRemoteFileNode> &node, std::function<void(int error)> callback);
 
-	static const struct fuse_lowlevel_ops fuse_ll_ops;
-
-	// Prevent the Application from quitting
+	/** Prevent the Application from quitting. */
 	std::unique_ptr<QEventLoopLocker> m_eventLoopLocker;
 
+	/** Struct of implemented fuse operations. */
+	static const struct fuse_lowlevel_ops fuse_ll_ops;
+
+	/** Fuse bookkeeping. */
 	struct fuse_session *m_fuseSession = nullptr;
+	/** Fuse bookkeeping. */
 	std::unique_ptr<QSocketNotifier> m_fuseNotifier;
 
-	// Set of nodes that need flushing
-	std::set<fuse_ino_t> m_dirtyNodes;
-
-	// Might not actually be free, so check m_nodes first
+	/** Used by insertNode for accelerating the search for the next free inode number. */
 	fuse_ino_t m_nextIno = KIOFuseIno::DynamicStart;
-	// Map of all known inodes to KIOFuseNodes
+	/** Map of all known inode numbers to KIOFuseNodes. */
 	std::unordered_map<fuse_ino_t, std::shared_ptr<KIOFuseNode>> m_nodes;
+	/** Set of all nodes with a dirty cache. */
+	std::set<fuse_ino_t> m_dirtyNodes;
 };
