2032853be1a3fe9a4d64dff35576188dc81143d8
Chinmoy Pradhan <chinmoy.pradhan@machinesoul.in>
2026-06-02 20:24:01 +0530

    Remove redundant check

--- src/kiofusevfs.cpp
+++ src/kiofusevfs.cpp
@@ -1614,8 +1614,7 @@ void KIOFuseVFS::lookup(fuse_req_t req, fuse_ino_t parent, const char *name)
 			return;
 		}
 
-		if(parentDirNode->m_parentIno == KIOFuseIno::Root
-		   && !parentDirNode->m_nodeName.isEmpty())
+		if(parentDirNode->m_parentIno == KIOFuseIno::Root)
 		{
 			const QString scheme = parentDirNode->m_nodeName;
 			const QString authority = nodeName;
